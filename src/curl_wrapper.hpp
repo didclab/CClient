1540b492ac4ab13179ab3b31ad29ccc95039424d
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-06-09 12:31:22 -0400

    Added curl_wrapper.hpp, a file defining get and post methods.

---
+++ src/curl_wrapper.hpp
@@ -0,0 +1,25 @@
+/*
+ * curl_wrapper.hpp
+ * Andrew Mikalsen
+ * 6/5/20
+ */
+
+#ifndef CURL_WRAPPER_HPP_INCLUDED
+#define CURL_WRAPPER_HPP_INCLUDED
+
+#include <string>
+
+namespace onedatashare {
+    /**
+     * Performs a GET request to the specified url with the specified headers.
+     * 
+     * @arg url string containing url to make the GET request to. Should ideally contain the protocol
+     * @arg headers vector of strings used as headers for the GET request
+     * 
+     * @return response from the request
+     */
+    std::string get(const std::string& url, const std::vector<std::string>& headers);
+    std::string post(const std::string& url, const std::vector<std::string>& headers, const std::string& data);
+}
+
+#endif // CURL_WRAPPER_HPP_INCLUDED
