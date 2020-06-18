e29f280d044716a5d815e5469008f8d2a87b3b2e
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-06-18 15:26:25 -0400

    Added one_data_share.hpp with class OneDataShare definition with constructor.

---
+++ src/one_data_share.hpp
@@ -0,0 +1,13 @@
+/*
+ * one_data_share.hpp
+ * Andrew Mikalsen
+ * 6/18/20
+ */
+
+#include <string>
+
+namespace ods {
+    class OneDataShare {
+        OneDataShare(std::string token);
+    };
+}
