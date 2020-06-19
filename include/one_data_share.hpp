c688df288f8298d3d9543e44548b8350a8083917
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-06-19 11:40:21 -0400

    Moved all definitions into one_data_share.hpp. They can be moved into separate headers as needed later on. Stubbed out basic class definitions for class heirarchy.

--- include/one_data_share.hpp
+++ include/one_data_share.hpp
@@ -7,30 +7,55 @@
 #ifndef ONE_DATA_SHARE_INCLUDED
 #define ONE_DATA_SHARE_INCLUDED
 
-#include <string>
-#include "transfer.hpp"
-
 namespace ods {
-    /**
-     * Enables the instantiation of endpoints and initialization of transfers.
-     * 
-     * This class is the main entry point to the One Data Share client. Any available functionality from One Data Share
-     * is provided through an instance of this class.
-     */
     class OneDataShare {
-
         public:
-            /**
-             * Creates a new <code>OneDataShare</code> instance with the specified token. The token must be obtained from the One Data
-             * Share website.
-             * 
-             * @param token JWT token used to authenticate with One Data Share
-             */
-            OneDataShare(std::string token);
-            OneDataShare(const OneDataShare&) = delete;
-            OneDataShare& operator=(const OneDataShare&) = delete;
+        private:
+    };
 
-            Transfer transfer();
+    class Transfer: public OneDataShare {
+        public:
+        private:
+    };
+
+    class Endpoint: public OneDataShare {
+        public:
+        private:
+    };
+
+    class Dropbox: public Endpoint {
+        public:
+        private:
+    };
+
+    class Box: public Endpoint {
+        public:
+        private:
+    };
+
+    class GoogleDrive: public Endpoint {
+        public:
+        private:
+    };
+
+    class GridFTP: public Endpoint {
+        public:
+        private:
+    };
+
+    class FTP: public Endpoint {
+        public:
+        private:
+    };
+
+    class SFTP: public Endpoint {
+        public:
+        private:
+    };
+
+    class HTTP: public Endpoint {
+        public:
+        private:
     };
 }
 
