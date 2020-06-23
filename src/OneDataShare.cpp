c876f96012344320acd8ccd9082ea55eb19a8dde
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-06-23 16:12:30 -0400

    Added OneDataShare.cpp for OneDataShare class.

---
+++ src/OneDataShare.cpp
@@ -0,0 +1,12 @@
+/*
+ * OneDataShare.cpp
+ * Andrew Mikalsen
+ * 6/23/20
+ */
+
+#include <OneDataShare.hpp>
+
+namespace ods {
+    OneDataShare::OneDataShare(const std::string ods_auth_token) : _ods_auth_token(ods_auth_token) {
+    }
+}
