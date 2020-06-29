837a27d7a5cee0f391f2bcaa96cd477707856b1c
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-06-29 13:52:07 -0400

    Added a basic declaration of Item in Item.hpp. Added include guards to Item.hpp.

--- include/Item.hpp
+++ include/Item.hpp
@@ -4,6 +4,19 @@
  * 6/29/20
  */
 
+#ifndef ITEM_HPP_INCLUDED
+#define ITEM_HPP_INCLUDED
+
 namespace ods {
-    
+    /**
+     * Lazily-evaluated handle to an item found at an endpoint.
+     */
+    class Item {
+        Item() = default;
+        Item(const Item&) = delete;
+        Item& operator=(const Item&) = delete;
+        virtual ~Item() = 0;
+    };
 }
+
+#endif // ITEM_HPP_INCLUDED
