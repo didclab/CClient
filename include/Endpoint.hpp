25f6f4228dc317d4ca0669e27a587b247d4b53c1
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-06-29 13:46:55 -0400

    Added include guards to Endpoint.hpp, made Endpoint constructor the default constructor, made Endpoint destructor a pure virtual function.

--- include/Endpoint.hpp
+++ include/Endpoint.hpp
@@ -4,14 +4,19 @@
  * 6/29/20
  */
 
+#ifndef ENDPOINT_HPP_INCLUDED
+#define ENDPOINT_HPP_INCLUDED
+
 namespace ods {
     /**
      * Connection to a One Data Share endpoint.
      */
     class Endpoint {
-        Endpoint();
+        Endpoint() = default;
         Endpoint(const Endpoint&) = delete;
         Endpoint& operator=(const Endpoint&) = delete;
-        virtual ~Endpoint();
+        virtual ~Endpoint() = 0;
     };
 }
+
+#endif // ENDPOINT_HPP_INCLUDED
