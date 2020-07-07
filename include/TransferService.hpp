9ac68def0b66338a3f3acdfba80979fee91e7034
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 16:41:26 -0400

    Added doc comments for TransferService.

--- include/TransferService.hpp
+++ include/TransferService.hpp
@@ -15,12 +15,44 @@
 #include <TransferStatus.hpp>
 
 namespace ods {
+    /**
+     * Service providing all functionality related to making transfers.
+     */
     class TransferService{
         public:
+            /**
+             * Creates a new TransferService object with the specified
+             * authentication token, passing ownership of the TransferService
+             * object to the caller.
+             * 
+             * @param ods_auth_token the one data share authentication token to
+             * use
+             * 
+             * @return a unique pointer to a new TransferService object
+             */
             static std::unique_ptr<TransferService> create(std::string ods_auth_token);
 
+            /**
+             * Starts a new transfer job from the specified source to the
+             * specified destination with the specified options.
+             * 
+             * @param source the source endpoint and the resources from that
+             * endpoint to transfer
+             * @param destination the destination endpoint and the directory
+             * to transfer the resources from the source to
+             * @param options the options to use for this transfer request
+             * 
+             * @return the id of the new transfer job
+             */
             virtual std::string transfer(Source source, Destination destination, TransferOptions options) = 0;
 
+            /**
+             * Checks the status of the specified transfer job
+             * 
+             * @param id the id of the transfer job to check
+             * 
+             * @return the status of the transfer job
+             */
             virtual TransferStatus status(std::string id) = 0;
 
             virtual ~TransferService() = 0;
