7e07fb774c08ca9ea6696689f915a91247370cf2
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 17:41:22 -0400

    Made Source::create take borrowed references to paramneters.

--- include/Source.hpp
+++ include/Source.hpp
@@ -22,19 +22,21 @@ namespace ods {
              * Creates a new Source object with the specified endpoint and
              * resources.
              * 
-             * @param type the endpoint type to transfer from
-             * @param cred_id the credential identifier of the endpoint to
-             * transfer from
-             * @param directory_identifier the path or id, depending on the
-             * endpoint type, that is needed in order to locate the directory
-             * containing the resources to transfer
-             * @param resource_identifiers the names or ids, depending on the
-             * endpoint type, that are needed in order to locate the resources
-             * to transfer from within the specified directory
+             * @param type borrowed reference to the endpoint type to transfer
+             * from
+             * @param cred_id borrowed reference to the credential identifier of
+             * the endpoint to transfer from
+             * @param directory_identifier borrowed reference to the path or id,
+             * depending on the endpoint type, that is needed in order to locate
+             * the directory containing the resources to transfer
+             * @param resource_identifiers borrowed reference to the the names
+             * or ids, depending on the endpoint type, that are needed in order
+             * to locate the resources to transfer from within the specified
+             * directory
              * 
              * @return a unique pointer to a new Source object
              */
-            static std::unique_ptr<Source> create(EndpointType type, std::string cred_id, std::string directory_identifier, std::vector<std::string> resource_identifiers);
+            static std::unique_ptr<Source> create(const EndpointType& type, const std::string& cred_id, const std::string& directory_identifier, const std::vector<std::string>& resource_identifiers);
 
             virtual ~Source() = 0;
         protected:
