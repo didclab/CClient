ce9475acafee0116714e324e2b478b7c6a63700e
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 17:08:12 -0400

    CredentialService methods now take const borrowed references.

--- include/CredentialService.hpp
+++ include/CredentialService.hpp
@@ -42,50 +42,54 @@ namespace ods {
              * authentication token, passing ownership of the CredentialService
              * object to the caller.
              * 
-             * @param ods_auth_token the one data share authentication token to
-             * use
+             * @param ods_auth_token borrowed reference to the One Data Share
+             * authentication token to use
              * 
              * @return a unique pointer to a new CredentialService object
              */
-            static std::unique_ptr<CredentialService> create(std::string ods_auth_token);
+            static std::unique_ptr<CredentialService> create(const std::string& ods_auth_token);
 
             /**
              * Gets the url that can be used to register an endpoint of the
              * specified endpoint type with One Data Share via OAuth.
              * 
-             * @param type the endpoint type to get the OAuth url for
+             * @param type borrowed reference to the endpoint type to get the
+             * OAuth url for
              * 
              * @return a string containing the OAuth url
              */
-            virtual std::string oauth_link(OAuthEndpointType type) const = 0;
+            virtual std::string oauth_link(const OAuthEndpointType& type) const = 0;
 
             /**
              * Registers the specified endpoint with One Data Share using the
              * specified credentials.
              * 
-             * @param type the endpoint type to register
-             * @param cred_id the credential identifier used to refer to the
-             * registered endpoint
-             * @param uri the uri of the endpoint to register
-             * @param username the username needed to log in to the endpoint
-             * @param secret the password needed to log in to the endpoint
+             * @param type borrowed reference to the endpoint type to register
+             * @param cred_id borrowed reference to the credential identifier
+             * to associate with the registered endpoint
+             * @param uri borrowed reference to the uri of the endpoint to
+             * register
+             * @param username borrowed reference to the username needed to log
+             * in to the endpoint
+             * @param secret borrowed reference to the password needed to log in
+             * to the endpoint
              * 
              * @return true if and only if this operation successfully
              * registered the endpoint with the specified credentials
              */
-            virtual bool register_credential(CredentialEndpointType type, std::string cred_id, std::string uri, std::string username, std::string secret) const = 0;
+            virtual bool register_credential(const CredentialEndpointType& type, const std::string& cred_id, const std::string& uri, const std::string& username, const std::string& secret) const = 0;
 
             /**
              * Lists the credential identifiers of the specified endpoint type
              * that are registered with One Data Share.
              * 
-             * @param type endpoint type to list the registered credential
-             * identifiers of
+             * @param type borrowed reference to the endpoint type to list the
+             * registered credential identifiers of
              * 
              * @return a vector of the registered credential identifiers for the
              * sepcified endpoint
              */
-            virtual std::vector<std::string> credential_id_list(EndpointType type) const = 0;
+            virtual std::vector<std::string> credential_id_list(const EndpointType& type) const = 0;
 
             CredentialService(const CredentialService&) = delete;
             CredentialService& operator=(const CredentialService&) = delete;
