99718cee0bf7d93066b4a8eef6a7206a772b0a8e
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 16:05:16 -0400

    Added cred_id to the parameters for SOurce::create and added doc comments for Source.

--- include/Source.hpp
+++ include/Source.hpp
@@ -13,9 +13,29 @@
 #include <EndpointType.hpp>
 
 namespace ods {
+    /**
+     * Indicates the source endpoint of a transfer.
+     */
     class Source {
         public:
-            static std::unique_ptr<Source> create(EndpointType tyope, std::string directory_identifier, std::vector<std::string> resource_identifiers);
+            /**
+             * Creates a new Source object with the specified endpoint and
+             * resources.
+             * 
+             * @param type the endpoint type to transfer from
+             * @param cred_id the credential identifier of the endpoint to
+             * transfer from
+             * @param directory_identifier the path or id, depending on the
+             * endpoint type, that is needed in order to locate the directory
+             * containing the resources to transfer
+             * @param resource_identifiers the names or ids, depending on the
+             * endpoint type, that are needed in order to locate the resources
+             * to transfer from within the specified directory
+             * 
+             * @return a unique pointer to a new Source object
+             */
+            static std::unique_ptr<Source> create(EndpointType type, std::string cred_id, std::string directory_identifier, std::vector<std::string> resource_identifiers);
+            
             virtual ~Source() = 0;
         protected:
             Source() = default;
