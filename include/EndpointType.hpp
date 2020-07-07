9c8bb29933c5b73d17932f5364dc7c8094c1aee8
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 08:54:53 -0400

    Readded EndpointType.hpp, added S3 to enum, added doc comment.

---
+++ include/EndpointType.hpp
@@ -0,0 +1,19 @@
+/*
+ * EndpointType.hpp
+ * Andrew Mikalsen
+ * 6/23/20
+ */
+
+#ifndef ENDPOINT_TYPE_HPP_INCLUDED
+#define ENDPOINT_TYPE_HPP_INCLUDED
+
+namespace ods {
+    /**
+     * Contains the possible types of an Endpoint.
+     */
+    enum class EndpointType {
+        DROPBOX, GOOGLE_DRIVE, SFTP, FTP, BOX, S3, GFTP, HTTP
+    };
+}
+
+#endif // ENDPOINT_TYPE_HPP_INCLUDED
