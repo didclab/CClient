98e61d59a09e3bc2038eda72281e96522edcbcc9
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 16:09:07 -0400

    Added Destination.hpp.

---
+++ include/Destination.hpp
@@ -0,0 +1,17 @@
+/*
+ * Destination.hpp
+ * Andrew Mikalsen
+ * 7/7/20
+ */
+
+#ifndef DESTINATION_HPP_INCLUDED
+#define DESTINATION_HPP_INCLUDED
+
+namespace ods {
+    class Destination {
+        public:
+        protected:
+    };
+}
+
+#endif // DESTINATION_HPP_INCLUDED
