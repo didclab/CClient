61ea1792c15af534cbeeff1f20ff1fbdeeaeb28e
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 16:33:51 -0400

    Added empty class TransferStatus.

---
+++ include/TransferStatus.hpp
@@ -0,0 +1,17 @@
+/*
+ * TransferStatus.hpp
+ * Andrew Mikalsen
+ * 7/7/20
+ */
+
+#ifndef TRANSFER_STATUS_HPP_INCLUDED
+#define TRANSFER_STATUS_HPP_INCLUDED
+
+namespace ods {
+
+    // TODO: define
+    class TransferStatus {
+    };
+}
+
+#endif // TRANSFER_STATUS_HPP_INCLUDED
