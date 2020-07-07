cc106a59e68bd6862a30399b70d3171087eddd78
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 15:16:19 -0400

    Added missing semicolon in Source.hpp and added Source::create.

--- include/Source.hpp
+++ include/Source.hpp
@@ -7,12 +7,18 @@
 #ifndef SOURCE_HPP_INCLUDED
 #define SOURCE_HPP_INCLUDED
 
+#include <memory>
+#include <string>
+#include <vector>
+#include <EndpointType.hpp>
+
 namespace ods {
     class Source {
         public:
+            static std::unique_ptr<Source> create(EndpointType tyope, std::string directory_identifier, std::vector<std::string> resource_identifiers);
             virtual ~Source() = 0;
         protected:
-            Source() = default
+            Source() = default;
     };
 }
 
