cd142dae4d6cd4c1bac05ae00ddd2d5f6394779f
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 15:11:14 -0400

    Created Source.hpp.

---
+++ include/Source.hpp
@@ -0,0 +1,17 @@
+/**
+ * Source.hpp
+ * Andrew Mikalsen
+ * 7/7/20
+ */
+
+#ifndef SOURCE_HPP_INCLUDED
+#define SOURCE_HPP_INCLUDED
+
+namespace ods {
+    class Source {
+        public:
+        protected:
+    };
+}
+
+#endif // SOURCE_HPP_INCLUDED
