8ac7cdc8333f210a8b415409cf245d5dd25470dc
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 16:25:28 -0400

    Added TransferService::create.

--- include/TransferService.hpp
+++ include/TransferService.hpp
@@ -7,9 +7,14 @@
 #ifndef TRANSFER_SERVICE_HPP_INCLUDED
 #define TRANSFER_SERVICE_HPP_INCLUDED
 
+#include <memory>
+#include <string>
+
 namespace ods {
     class TransferService{
         public:
+            static std::unique_ptr<TransferService> create(std::string ods_auth_token);
+
             virtual ~TransferService() = 0;
         protected:
             TransferService() = default;
