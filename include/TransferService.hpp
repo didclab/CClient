45ff105f0d57248709b5f54aaf57ccc0582b2d4a
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 16:23:38 -0400

    Added TransferService.hpp

---
+++ include/TransferService.hpp
@@ -0,0 +1,17 @@
+/*
+ * TransferService.hpp
+ * Andrew Mikalsen
+ * 7/7/20
+ */
+
+#ifndef TRANSFER_SERVICE_HPP_INCLUDED
+#define TRANSFER_SERVICE_HPP_INCLUDED
+
+namespace ods {
+    class TransferService{
+        public:
+        protected:
+    };
+}
+
+#endif // TRANSFER_SERVICE_HPP_INCLUDED
