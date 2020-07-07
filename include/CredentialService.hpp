d9a0d5878c49fba9667f18dfdb93bd2a47669908
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-07 14:25:18 -0400

    Added CredentialService.hpp to include/.

---
+++ include/CredentialService.hpp
@@ -0,0 +1,17 @@
+/*
+ * CredentialService.hpp
+ * Andrew Mikalsen
+ * 7/7/20
+ */
+
+#ifndef CREDENTIAL_SERVICE_HPP_INCLUDED
+#define CREDENTIAL_SERVICE_HPP_INCLUDED
+
+namespace ods {
+    class CredentialService {
+        public:
+        private:
+    };
+}
+
+#endif // CREDENTIAL_SERVICE_HPP_INCLUDED
