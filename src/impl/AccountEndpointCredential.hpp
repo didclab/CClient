5ba03a194632013281f7516239dd92e841099227
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-09 15:38:51 -0400

    Added AccountEndpointCredential.hpp and CredentialServiceImpl.hpp.

---
+++ src/impl/AccountEndpointCredential.hpp
@@ -0,0 +1,13 @@
+/*
+ * AccountEndpointCredential.hpp
+ * Andrew Mikalsen
+ * 7/9/20
+ */
+
+namespace ods {
+    namespace impl {
+        class AccountEndpointCredential {
+
+        };
+    }
+}
---
+++ src/impl/CredentialServiceImpl.hpp
@@ -0,0 +1,15 @@
+/*
+ * CredentialServiceImpl.hpp
+ * Andrew Mikalsen
+ * 7/9/20
+ */
+
+#include <CredentialService.hpp>
+
+namespace ods {
+    namespace impl {
+        class CredentialServiceImpl {
+
+        };
+    }
+}
