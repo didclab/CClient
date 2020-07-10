84eecbcd751a52fbcf15b295e222705a7c951105
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-10 17:18:23 -0400

    Changed constructor of CredentialServiceImpl to take the url and Rest object needed to make REST API requests to ODS.

--- src/internal/CredentialServiceImpl.hpp
+++ src/internal/CredentialServiceImpl.hpp
@@ -4,7 +4,9 @@
  * 7/9/20
  */
 
+#include <memory>
 #include <CredentialService.hpp>
+#include <Rest.hpp>
 
 namespace ods {
     namespace internal {
@@ -16,13 +18,32 @@ namespace ods {
             public:
                 /**
                  * Creates a new CredentialServiceImpl object with the specified
-                 * One Data Share authentication token.
+                 * token, url, and rest caller.
+                 * 
+                 * @param ods_auth_token token to use for REST API calls
+                 * @param ods_url url to make REST API calls to
+                 * @param rest_caller object to use for making REST API calls
                  */
-                CredentialServiceImpl(std::string ods_auth_token);
+                CredentialServiceImpl(std::string ods_auth_token, std::string ods_url, std::unique_ptr<rest::Rest> rest_caller);
                 virtual std::string oauth_url(OAuthEndpointType type) const override;
                 virtual bool register_credential(CredentialEndpointType type, const std::string& cred_id, const std::string& uri, const std::string& username, const std::string& secret) const override;
                 virtual std::vector<std::string> credential_id_list(EndpointType type) const override;
                 virtual ~CredentialServiceImpl() override;
+            private:
+                /**
+                 * One Data Share authentication token used in REST API calls.
+                 */
+                const std::string _ods_auth_token;
+
+                /**
+                 * Url to the One Data Share server to make REST API calls to.
+                 */
+                const std::string _ods_url;
+
+                /**
+                 * Pointer to the object used to make REST API calls.
+                 */
+                const std::unique_ptr<rest::Rest> _rest_caller;
         };
     }
 }
