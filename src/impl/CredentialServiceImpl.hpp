dfcb7d440cfd63a1790bbe71b204c2de1ec80271
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-10 08:50:41 -0400

    Made CredentialServiceImpl override CredentialService and aoverrided abstract methods.

--- src/impl/CredentialServiceImpl.hpp
+++ src/impl/CredentialServiceImpl.hpp
@@ -8,8 +8,21 @@
 
 namespace ods {
     namespace impl {
-        class CredentialServiceImpl {
-
+        /**
+         * Service that makes REST API calls to One Data Share related to
+         * registering credentials.
+         */
+        class CredentialServiceImpl: public CredentialService {
+            public:
+                /**
+                 * Creates a new CredentialServiceImpl object with the specified
+                 * One Data Share authentication token.
+                 */
+                CredentialServiceImpl(std::string ods_auth_token);
+                virtual std::string oauth_url(const OAuthEndpointType type) const override;
+                virtual bool register_credential(const CredentialEndpointType type, const std::string& cred_id, const std::string& uri, const std::string& username, const std::string& secret) const override;
+                virtual std::vector<std::string> credential_id_list(const EndpointType type) const override;
+                virtual ~CredentialServiceImpl() override = default;
         };
     }
 }
