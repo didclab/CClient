01f7bc22afa50ffffe2dbe8f8905009ca73c994e
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-10 15:46:27 -0400

    Removed comment on rest namespace, edited doc comments and reordered some functions in Rest.hpp.

--- src/rest/Rest.hpp
+++ src/rest/Rest.hpp
@@ -10,9 +10,6 @@
 #include "Response.hpp"
 
 namespace ods {
-    /**
-     * Provides functions for making REST API calls.
-     */
     namespace rest {
         /**
          * Class used to perform REST requests.
@@ -20,27 +17,40 @@ namespace ods {
         class Rest {
             public:
                 Rest() = default;
-                Rest(const Rest&) = delete;
-                Rest& operator=(const Rest&) = delete;
+
 			    /**
-				 * Performs a GET request to the specified url with the specified headers.
+				 * Performs a GET request to the specified url with the
+				 * specified headers.
 				 * 
-				 * @param url string containing url to make the GET request to. Should ideally contain the protocol
-				 * @param headers multi-map containing the headers for the GET request
+				 * @param url borrowed reference to the string containing url to
+				 * make the GET request to. Should ideally contain the protocol
+				 * @param headers borrowed reference to the multi-map containing
+				 * the headers for the GET request
 				 * 
-				 * @return Response object containing the response headers, body, and http status
+				 * @return Response object containing the response headers,
+				 * body, and http status
 				 */
 				virtual Response get(const std::string& url, const std::unordered_multimap<std::string, std::string>& headers) const = 0;
                 /**
-				 * Performs a POST request to the specified url with the specified headers and data.
+				 * Performs a POST request to the specified url with the
+				 * specified headers and data.
 				 * 
-				 * @param url string containing the url to make the POST request to. Should ideally contain the protocol
-				 * @param headers multi-map containing the headers for the POST request
-				 * @param data string containing the json data for the POST request
+				 * @param url borrowed reference to the string containing the
+				 * url to make the POST request to, ideally containing the
+				 * protocol
+				 * @param headers borrowed reference to the multi-map containing
+				 * the headers for the POST request
+				 * @param data borrowed reference to the string containing the
+				 * json data for the POST request
 				 * 
-				 * @return Response object containing the response headers, body, and http status
+				 * @return the Response object containing the response headers,
+				 * body, and http status
 				 */
 				virtual Response post(const std::string& url, const std::unordered_multimap<std::string, std::string>& headers, const std::string& data) const = 0;
+
+                Rest(const Rest&) = delete;
+                Rest& operator=(const Rest&) = delete;
+
 				virtual ~Rest() = 0;
         };
     }
