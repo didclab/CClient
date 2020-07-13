9efdef8cd41328d5c2c53d1d7e9027c2668f333f
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-13 12:23:25 -0400

    Added constructor implementation for CredentialServiceImpl.

--- src/internal/CredentialServiceImpl.cpp
+++ src/internal/CredentialServiceImpl.cpp
@@ -6,8 +6,15 @@
 
 #include "CredentialServiceImpl.hpp"
 
+#include <utility>
+
 namespace ods {
     namespace internal {
+        CredentialServiceImpl::CredentialServiceImpl(const std::string& ods_auth_token, const std::string& ods_url, std::unique_ptr<rest::Rest> rest_caller) :
+            _ods_auth_token(ods_auth_token),
+            _ods_url(ods_url),
+            _rest_caller(std::move(rest_caller)) {
+        }
         CredentialServiceImpl::~CredentialServiceImpl() = default;
     }
 }
