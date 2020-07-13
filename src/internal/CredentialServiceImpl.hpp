3e19531f1841486eb69cd7dfe52c7230e318ccf5
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-13 12:04:40 -0400

    CredentialServiceImpl constructor now takes params by const ref.

--- src/internal/CredentialServiceImpl.hpp
+++ src/internal/CredentialServiceImpl.hpp
@@ -20,11 +20,13 @@ namespace ods {
                  * Creates a new CredentialServiceImpl object with the specified
                  * token, url, and rest caller.
                  * 
-                 * @param ods_auth_token token to use for REST API calls
-                 * @param ods_url url to make REST API calls to
+                 * @param ods_auth_token borrowed reference to the token to use
+                 * for REST API calls
+                 * @param ods_url borrowed reference to the url to make REST API
+                 * calls to
                  * @param rest_caller object to use for making REST API calls
                  */
-                CredentialServiceImpl(std::string ods_auth_token, std::string ods_url, std::unique_ptr<rest::Rest> rest_caller);
+                CredentialServiceImpl(const std::string& ods_auth_token, const std::string& ods_url, std::unique_ptr<rest::Rest> rest_caller);
                 virtual std::string oauth_url(OAuthEndpointType type) const override;
                 virtual bool register_credential(CredentialEndpointType type, const std::string& cred_id, const std::string& uri, const std::string& username, const std::string& secret) const override;
                 virtual std::vector<std::string> credential_id_list(EndpointType type) const override;
