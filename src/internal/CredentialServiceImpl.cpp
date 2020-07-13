b0b5ec51ee01f464b1886573979cecae862e1504
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-13 14:05:55 -0400

    Added constants for api paths to CredentialServiceImpl.

--- src/internal/CredentialServiceImpl.cpp
+++ src/internal/CredentialServiceImpl.cpp
@@ -10,11 +10,25 @@
 
 namespace ods {
     namespace internal {
+
+        namespace {
+            
+        }
+
+        const std::string CredentialServiceImpl::API_PATH_OAUTH("/api/oauth");
+        const std::string CredentialServiceImpl::API_PATH_CRED("/api/cred");
+
+
         CredentialServiceImpl::CredentialServiceImpl(const std::string& ods_auth_token, const std::string& ods_url, std::unique_ptr<rest::Rest> rest_caller) :
             _ods_auth_token(ods_auth_token),
             _ods_url(ods_url),
             _rest_caller(std::move(rest_caller)) {
         }
+
+        std::string CredentialService::oauth_url(OAuthEndpointType type) const {
+            
+        }
+
         CredentialServiceImpl::~CredentialServiceImpl() = default;
     }
 }
