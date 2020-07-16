8773d1dafc425cd80cde4faf8db290d4d9e908ff
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-16 10:32:36 -0400

    Added IOException and UnexpectedResponseException.

---
+++ include/IOException.hpp
@@ -0,0 +1,24 @@
+/*
+ * IOException.hpp
+ * Andrew Mikalsen
+ * 7/15/20
+ */
+
+#ifndef IO_EXCEPTION_HPP_INCLUDED
+#define IO_EXCEPTION_HPP_INCLUDED
+
+#include <stdexcept>
+
+namespace ods {
+    /**
+     * Exception thrown when an I/O operation fails.
+     */
+    class IOException : std::runtime_error {
+        public:
+            IOException(const std::string& what_arg);
+            IOException(const char* what_arg);
+            virtual ~IOException();
+    };
+}
+
+#endif // IO_EXCEPTION_HPP_INCLUDED
