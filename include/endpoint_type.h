95f42e74d937e89362c21e8c8f3dfb33b40fa52e
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-17 10:39:36 -0400

    Updated style for credential service and endpoint type.

---
+++ include/endpoint_type.h
@@ -0,0 +1,19 @@
+/*
+ * endpoint_type.h
+ * Andrew Mikalsen
+ * 6/23/20
+ */
+
+#ifndef ONEDATASHARE_ENDPOINT_TYPE_H
+#define ONEDATASHARE_ENDPOINT_TYPE_H
+
+namespace One_data_share {
+    /**
+     * Contains the possible types of an endpoint.
+     */
+    enum class Endpoint_type {
+        dropbox, google_drive, sftp, ftp, box, s3, gftp, http
+    };
+}
+
+#endif // ONEDATASHARE_ENDPOINT_TYPE_H
