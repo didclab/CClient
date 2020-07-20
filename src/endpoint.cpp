07ae9e4b2ddf03519e41179cee90c716c57493f4
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-20 16:30:55 -0400

    Stubbed out Resource in endpoint.cpp.

--- src/endpoint.cpp
+++ src/endpoint.cpp
@@ -8,4 +8,62 @@
 
 namespace One_data_share {
 
+    Resource::Resource() = default;
+
+    Resource::~Resource() = default;
+
+    std::shared_ptr<const std::string> Resource::id() const
+    {
+        // TODO: implement
+        return nullptr;
+    }
+
+    std::string Resource::name() const
+    {
+        // TODO: implement
+        return nullptr;
+    }
+
+    long Resource::size() const
+    {
+        // TODO: implement
+        return 0;
+    }
+
+    long Resource::time() const
+    {
+        // TODO: implement
+        return 0;
+    }
+
+    bool Resource::is_directory() const
+    {
+        // TODO: implement
+        return false;
+    }
+
+    bool Resource::is_file() const
+    {
+        // TODO: implement
+        return false;
+    }
+
+    std::shared_ptr<const std::string> Resource::link() const
+    {
+        // TODO: implement
+        return nullptr;
+    }
+
+    std::shared_ptr<const std::string> Resource::permissions() const
+    {
+        // TODO: implement
+        return nullptr;
+    }
+
+    std::shared_ptr<const std::vector<std::shared_ptr<const Resource>>> Resource::contained_resources() const
+    {
+        // TODO: implement
+        return nullptr;
+    }
+
 }
