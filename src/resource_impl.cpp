b83d1dfa60ad8ae5da71feb083202781f715ff7d
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-20 18:04:48 -0400

    Added resource_impl.cpp,h

---
+++ src/resource_impl.cpp
@@ -0,0 +1,11 @@
+/*
+ * resource_impl.cpp
+ * Andrew Mikalsen
+ * 7/20/20
+ */
+
+#include "resource_impl.h"
+
+namespace One_data_share {
+
+}
---
+++ src/resource_impl.h
@@ -0,0 +1,16 @@
+/*
+ * resource_impl.h
+ * Andrew Mikalsen
+ * 7/20/20
+ */
+
+#ifndef ONEDATASHARE_RESOURCE_IMPL_H
+#define ONEDATASHARE_RESOURCE_IMPL_H
+
+#include <onedatashare/endpoint.h>
+
+namespace One_data_share {
+
+}
+
+#endif // ONEDATASHARE_RESOURCE_IMPL_H
