89dc09bbd0c2937d476ade4d93e54bebb40afb6a
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-20 14:58:17 -0400

    Updated specification for Endpoint.

--- include/onedatashare/endpoint.h
+++ include/onedatashare/endpoint.h
@@ -146,7 +146,8 @@ namespace One_data_share {
     public:
         /**
          * Creates a new Endpoint object of the specified type with the specifed credential id and authentication token,
-         * passing ownership of the Endpoint object to the caller.
+         * passing ownership of the Endpoint object to the caller. It is expected that the specified authentication
+         * token is valid and that the specified credential id is registered with One Data Share.
          *
          * @param type the type of endpoint to return
          * @param cred_id borrowed reference to the credential id of the endpoint to use
@@ -169,53 +170,69 @@ namespace One_data_share {
         Endpoint& operator=(Endpoint&&) = default;
 
         /**
-         * Creates a Resource object corresponding to the resource found at the specified location if the endpoint could
-         * find the resource. The user is expected to check for a null pointer unless they are absolutely certain that
-         * the resource was found. Ownership of the returned Resource object is passed to the caller.
+         * Creates a Resource object corresponding to the resource found at the specified location. Ownership of the
+         * returned Resource object is passed to the caller. It is expected that the authentication token used to create
+         * this Endpoint object is valid, that a connection can be made to One Data Share, that a connection can be made
+         * from One Data Share to the endpoint specified by the credential id, and that the specified resource exists at
+         * the specified location. If these preconditions are not met, exceptions may be thrown.
          *
          * @param identifier borrowed reference to the path or id, dependending on the endpoint type, that the endpoint
          * needs in order to locate the resource
          *
-         * @return a unique pointer to the created Resource or a null pointer if no resource was found
+         * @return a unique pointer to the created Resource
+         *
+         * @exception Connection_error if unable to connect to One Data Share
+         * @exception Unexpected_response_error if an unexpected response is received from One Data Share
          */
         virtual std::unique_ptr<Resource> list(const std::string& identifier) const = 0;
 
         /**
-         * Removes the specified resource from the endpoint if the resource was found.
+         * Removes the specified resource from the endpoint. It is expected that the authentication token used to create
+         * this Endpoint object is valid, that a connection can be made to One Data Share, that a connection can be made
+         * from One Data Share to the endpoint specified by the credential id, and that the specified resource exists at
+         * the specified location. If these preconditions are not met, exceptions may be thrown.
          *
          * @param identifier borrowed reference to the path or id, depending on the endpoint type, that the endpoint
          * needs in order to locate the directory containing the resource to remove
          * @param to_delete borrowed reference to the name or id, depending on the endpoint type, that the endpoint
          * needs in order to locate the resource to remove from within the specified directory
          *
-         * @return true if and only if this operation successfully removed the specified resource from the endpoint
+         * @exception Connection_error if unable to connect to One Data Share
+         * @exception Unexpected_response_error if an unexpected response is received from One Data Share
          */
-        virtual bool remove(const std::string& identifier, const std::string& to_delete) const = 0;
+        virtual void remove(const std::string& identifier, const std::string& to_delete) const = 0;
 
         /**
-         * Creates a new directory with the specified name under the specified directory. The specified directory must
-         * exist. If the specified resource is not a directory, false is returned.
+         * Creates a new directory with the specified name under the specified directory. It is expected that the
+         * authentication token used to create this Endpoint object is valid, that a conenction can be made to One Data
+         * Share, that a connection can be made from One Data Share to the endpoint specified by the credential id, and
+         * that the specified resource exists at the specidied location. If these preconditions are not met, exceptinos
+         * may be thrown.
          *
          * @param identifier borrowed reference to the path or id, depending on the endpoint type, that the endpoint
          * needs in order to locate the directory to create the new directory under
          * @param folder_to_create borrowed reference to the name of the directory to create
          *
-         * @return true if and only if this operation successfully created the the new directory under the specified
-         * directory
+         * @exception Connection_error if unable to connect to One Data Share
+         * @exception Unexpected_response_error if an unexpected response is received from One Data Share
          */
-        virtual bool mkdir(const std::string& identifier, const std::string& folder_to_create) const = 0;
+        virtual void mkdir(const std::string& identifier, const std::string& folder_to_create) const = 0;
 
         /**
-         * Downloads the specified resource if the resource was found.
+         * Downloads the specified resource. It is expected that the authentication token used to create this Endpoint
+         * object is valid, that a conenction can be made to One Data Share, that a connection can be made from One Data
+         * Share to the endpoint specified by the credential id, and that the specified resource exists at the specidied
+         * location. If these preconditions are not met, exceptions may be thrown.
          *
          * @param identifier borrowed reference to the path or id, depending on the endpoint type, that the endpoint
          * needs in order to locate the directory containing the resource to download
          * @param file_to_download borrowed reference to the name or id depending on the endpoint type, that the
          * endpoint needs in order to locate the resource to download from within the specified directory
          *
-         * @return true if and only if this operation successfully downloaded the specified resource from the endpoint
+         * @exception Connection_error if unable to connect to One Data Share
+         * @exception Unexpected_response_error if an unexpected response is received from One Data Share
          */
-        virtual bool download(const std::string& identifier, const std::string& file_to_download) const = 0;
+        virtual void download(const std::string& identifier, const std::string& file_to_download) const = 0;
 
     protected:
         Endpoint();
