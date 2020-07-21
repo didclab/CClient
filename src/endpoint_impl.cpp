836e3f17fdbf7e1dd7010d0678f381ad2ace24f5
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-21 11:07:46 -0400

    Added implementation for Endpoint_impl ctor.

--- src/endpoint_impl.cpp
+++ src/endpoint_impl.cpp
@@ -4,9 +4,23 @@
  * 7/20/20
  */
 
+#include <utility>
+
 #include "endpoint_impl.h"
 
 namespace One_data_share {
+    Endpoint_impl::Endpoint_impl(Endpoint_type type,
+                                 const std::string& cred_id,
+                                 const std::string& ods_oauth_token,
+                                 const std::string& ods_url,
+                                 std::unique_ptr<Rest> rest_caller)
+        : type_(type),
+          cred_id_(cred_id),
+          ods_auth_token_(ods_oauth_token),
+          ods_url_(ods_url),
+          rest_caller_(std::move(rest_caller))
+    {}
+
     std::unique_ptr<Resource> Endpoint_impl::list(const std::string& identifier) const
     {
         // TODO: implement
