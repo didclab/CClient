661f7549a1f3f9faf17d12d4f5aecc4297b433f0
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-21 13:25:09 -0400

    Added the rest of tests for Endpoint_impl, testing that a resource cannot be a directory and not have contained resources, and that the name, size, and time are properly initialized for a resource.

--- tests/endpoint_impl_tests.cpp
+++ tests/endpoint_impl_tests.cpp
@@ -59,7 +59,10 @@ TEST_F(Endpoint_impl_tests, ListThrowsConnectionErr)
     }
 }
 
-TEST_F(Endpoint_impl_tests, ListWithBadResponseCodeThrowsUnexpectedResponse)
+/**
+ * Tests that list throws an Unexpected_response_error when the response has a 500 status code
+ */
+TEST_F(Endpoint_impl_tests, ListWithBadStatusCodeThrowsUnexpectedResponse)
 {
     for (auto type : types) {
         // set up mock returning status 500 for every get
@@ -90,6 +93,9 @@ TEST_F(Endpoint_impl_tests, ListWithBadResponseBodyThrowsUnexpectedResponse)
     }
 }
 
+/**
+ * Tests that the Resource returned from list returns null from id when the stat object recieved doesn't have an id.
+ */
 TEST_F(Endpoint_impl_tests, ListResourceWithoutID)
 {
     std::string stat {R"({
@@ -124,6 +130,9 @@ TEST_F(Endpoint_impl_tests, ListResourceWithoutID)
     }
 }
 
+/**
+ * Tests that the Resource returned from list returns the id from id when the stat object recieved has an id.
+ */
 TEST_F(Endpoint_impl_tests, ListResourceWithID)
 {
     std::string id_value {R"("this is the id")"};
@@ -382,6 +391,82 @@ TEST_F(Endpoint_impl_tests, ListResourceWithContainedResources)
         auto files {resource->contained_resources()};
 
         ASSERT_NE(files, nullptr);
+    }
+}
+
+/**
+ * Tests that an Unexpected_response_error is thrown if a listed resource is a directory and it doesnt have contained
+ * resources.
+ */
+TEST_F(Endpoint_impl_tests, ListDirectoryWithoutContainedResourcesThrowsUnexpectedResponse)
+{
+    // NOTE: this needs to be tested because it is one of the guaranties provided by the SDK
+
+    std::string stat {R"({
+        "id": "string",
+        "name": "string",
+        "size": 0,
+        "time": 0,
+        "dir": true,
+        "file": false,
+        "link": "string",
+        "permissions: "string",
+        "filesList": [
+            null
+        ],
+        "total_size": 0,
+        "total_num": 0
+    })"};
+
+    for (auto type : types) {
+        // set up mock returning stat
+        auto caller {std::make_unique<Rest_mock>()};
+        EXPECT_CALL(*caller, get).WillOnce(Return(Ods::Response {Header_map {}, stat, 200}));
+
+        const Ods::Endpoint_impl endpoint {type, "", "", "", std::move(caller)};
+
+        EXPECT_THROW(endpoint.list(""), Ods::Unexpected_response_error);
+    }
+}
+
+/**
+ * Tests that listing a resource always returns the correct name, size, and time.
+ */
+TEST_F(Endpoint_impl_tests, ListReturnsValues)
+{
+    std::string name_val {R"("this is the name")"};
+    auto size_val = 139;
+    auto time_val = 24576;
+
+    std::string stat {R"({
+        "name": )" + name_val +
+                      R"(,
+        "size": )" + std::to_string(size_val) +
+                      R"(,
+        "time": )" + std::to_string(time_val) +
+                      R"(,
+        "dir": true,
+        "file": true,
+        "filesList": [
+            null
+        ],
+        "total_size": 0,
+        "total_num": 0
+    })"};
+
+    for (auto type : types) {
+        // set up mock returning stat
+        auto caller {std::make_unique<Rest_mock>()};
+        EXPECT_CALL(*caller, get).WillOnce(Return(Ods::Response {Header_map {}, stat, 200}));
+
+        const Ods::Endpoint_impl endpoint {type, "", "", "", std::move(caller)};
+
+        auto resource {endpoint.list("")};
+        ASSERT_NE(resource, nullptr);
+
+        EXPECT_EQ(resource->name(), name_val);
+        EXPECT_EQ(resource->size(), size_val);
+        EXPECT_EQ(resource->time(), time_val);
     }
 }
 
