71be8fd6bdad381631174e106c1f06d975647c54
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-22 12:15:52 -0400

    Updated examples.

--- examples/example_1.cpp
+++ examples/example_1.cpp
@@ -11,9 +11,10 @@
 #include <onedatashare/credential_service.h>
 #include <onedatashare/endpoint.h>
 #include <onedatashare/endpoint_type.h>
+#include <onedatashare/ods_error.h>
 
 /**
- * Registers a new FTP endpoint and lists the contents of its root directory.
+ * Example registering a new FTP endpoint and listing the contents of the root directory of that endpoint.
  */
 int main()
 {
@@ -33,26 +34,40 @@ int main()
     std::getline(token_file, token);
     token_file.close();
 
-    const std::string my_cred_id {"my_endpoint"};
-
-    // register new endpoint
+    const auto my_cred_id {"my_endpoint"};
     const auto cred_service {Ods::Credential_service::create(token)};
-    cred_service->register_credential(Ods::Credential_endpoint_type::ftp,
-                                      my_cred_id,
-                                      "ftp://speedtest.tele2.net",
-                                      nullptr,
-                                      nullptr);
 
-    // print oauth url for dropbox
-    std::cout << cred_service->oauth_url(Ods::Oauth_endpoint_type::dropbox) << std::endl;
+    try {
+        // register new endpoint
+        std::cout << "Registering new endpoint with credential id " << my_cred_id << " ..." << std::endl;
 
-    // print root of new endpoint
-    const auto my_endpoint {Ods::Endpoint::create(Ods::Endpoint_type::ftp, my_cred_id, token)};
-    const auto root {my_endpoint->list("/")};
-    if (root->is_directory()) {
-        for (auto r : *root->contained_resources()) {
-            std::cout << r->name() << std::endl;
+        cred_service->register_credential(Ods::Credential_endpoint_type::ftp,
+                                          my_cred_id,
+                                          "ftp://speedtest.tele2.net",
+                                          nullptr,
+                                          nullptr);
+
+        std::cout << "Endpoint registered.\n" << std::endl;
+
+        std::cout << "Printing root of " << my_cred_id << " ...\n" << std::endl;
+
+        // print root of new endpoint
+        const auto root_path {"/"};
+        const auto my_endpoint {Ods::Endpoint::create(Ods::Endpoint_type::ftp, my_cred_id, token)};
+        const auto root {my_endpoint->list(root_path)};
+        if (root->is_directory()) {
+            for (auto r : *root->contained_resources()) {
+                std::cout << r->name() << std::endl;
+            }
         }
+
+        std::cout << "\nRoot printed." << std::endl;
+
+    } catch (Ods::Unexpected_response_error e) {
+        std::cout << "\nUnexpected response received from One Data Share.\n\nwhat(): " << e.what()
+                  << "\n\nstatus: " << e.status << std::endl;
+    } catch (Ods::Connection_error e) {
+        std::cout << "\nError connecting to One Data Share.\n\nwhat(): " << e.what() << std::endl;
     }
 
     return 0;
