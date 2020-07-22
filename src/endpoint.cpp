38528a8dcba914d714630784233cb3542f7865a1
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-22 10:58:57 -0400

    Implemented Endpoint.

--- src/endpoint.cpp
+++ src/endpoint.cpp
@@ -6,6 +6,10 @@
 
 #include <onedatashare/endpoint.h>
 
+#include "endpoint_impl.h"
+#include "curl_rest.h"
+#include "utils.h"
+
 namespace One_data_share {
 
 Resource::Resource() = default;
@@ -16,8 +20,18 @@ std::unique_ptr<Endpoint> Endpoint::create(Endpoint_type type,
                                            const std::string& cred_id,
                                            const std::string& ods_auth_token)
 {
+    // TODO: make this false in production build
+    auto use_configured_ods_url = true;
+
+    std::string ods_url {};
+    if (use_configured_ods_url) {
+        load_url_from_config(ods_url);
+    } else {
+        ods_url = get_ods_production_url();
+    }
+
     // TODO: implement
-    return nullptr;
+    return std::make_unique<Endpoint_impl>(type, cred_id, ods_auth_token, ods_url, std::make_unique<Curl_rest>());
 }
 
 Endpoint::Endpoint() = default;
