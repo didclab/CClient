fc92ed32c159b44affac108e7e9abffd9fd7d4d9
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-23 13:05:15 -0400

    Added definition of Transfer_service_impl.

--- src/transfer_service_impl.h
+++ src/transfer_service_impl.h
@@ -7,11 +7,15 @@
 #ifndef ONEDATASHARE_TRANSFER_SERVICE_IMPL_H
 #define ONEDATASHARE_TRANSFER_SERVICE_IMPL_H
 
+#include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <onedatashare/endpoint_type.h>
 #include <onedatashare/transfer_service.h>
+
+#include "rest.h"
 
 namespace One_data_share {
 
@@ -19,7 +23,7 @@ class Destination_impl : public Destination {
 public:
     Destination_impl(Endpoint_type type, std::string cred_id, std::string directory_identifier);
 
-    ~Destination_impl() = default;
+    virtual ~Destination_impl() = default;
 
     Destination_impl(const Destination_impl&) = default;
     Destination_impl& operator=(const Destination_impl&) = default;
@@ -39,7 +43,7 @@ public:
                 const std::string& directory_identifier,
                 const std::vector<std::string>& resource_identifiers);
 
-    ~Source_impl() = default;
+    virtual ~Source_impl() = default;
 
     Source_impl(const Source_impl&) = default;
     Source_impl& operator=(const Source_impl&) = default;
@@ -47,22 +51,42 @@ public:
     Source_impl& operator=(Source_impl&&) = default;
 
 private:
+    const Endpoint_type type_;
+    const std::string cred_id_;
+    const std::string directory_identifier_;
+    const std::vector<std::string> resource_identifiers_;
 };
 
 class Transfer_options_impl : public Transfer_options {
 public:
     Transfer_options_impl() = default;
-    ~Transfer_options_impl() = default;
+    virtual ~Transfer_options_impl() = default;
 };
 
 class Transfer_status_impl : public Transfer_status {
     Transfer_status_impl() = default;
-    ~Transfer_status_impl() = default;
+    virtual ~Transfer_status_impl() = default;
 };
 
 class Transfer_service_impl : public Transfer_service {
 public:
+    Transfer_service_impl(const std::string& ods_auth_token,
+                          const std::string& ods_url,
+                          std::unique_ptr<Rest> rest_caller);
+
+    virtual ~Transfer_service_impl() = default;
+
+    virtual std::string transfer(const Source& source,
+                                 const Destination& destination,
+                                 const Transfer_options& options) const override;
+
+    virtual std::unique_ptr<Transfer_status> status(const std::string& id) const override;
+
 private:
+    const std::string ods_auth_token_;
+    const std::string ods_url_;
+    const std::unique_ptr<Rest> rest_caller_;
+    const std::unordered_multimap<std::string, std::string> headers_;
 };
 
 } // namespace One_data_share
