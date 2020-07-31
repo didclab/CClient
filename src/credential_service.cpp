3d04f6cf048f2073565b1a835963239388b34008
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-07-31 16:12:59 -0400

    Implemented new versions of create for Credential_service.

--- src/credential_service.cpp
+++ src/credential_service.cpp
@@ -14,17 +14,15 @@ namespace One_data_share {
 
 std::unique_ptr<Credential_service> Credential_service::create(const std::string& ods_auth_token)
 {
-    // TODO: make this false in production build
-    auto use_configured_ods_url = true;
+    return create(ods_auth_token, Internal::get_ods_production_url());
+}
 
-    std::string ods_url {};
-    if (use_configured_ods_url) {
-        Internal::load_url_from_config(ods_url);
-    } else {
-        ods_url = Internal::get_ods_production_url();
-    }
-
-    return std::make_unique<Internal::Credential_service_impl>(ods_auth_token, ods_url, std::make_unique<Internal::Curl_rest>());
+std::unique_ptr<Credential_service> Credential_service::create(const std::string& ods_auth_token,
+                                                               const std::string& url)
+{
+    return std::make_unique<Internal::Credential_service_impl>(ods_auth_token,
+                                                               url,
+                                                               std::make_unique<Internal::Curl_rest>());
 }
 
 Credential_service::Credential_service() = default;
