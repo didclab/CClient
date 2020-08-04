0786cd78cab3bf217b15035521ea8b74e0622fdd
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-08-04 15:44:07 -0400

    Added include guard for onedatashare.h.

--- include/onedatashare/onedatashare.h
+++ include/onedatashare/onedatashare.h
@@ -8,8 +8,13 @@
  * Includes all header files needed to use the One Data Share SDK.
  */
 
+#ifndef ONEDATASHARE_ONEDATASHARE_H
+#define ONEDATASHARE_ONEDATASHARE_H
+
 #include "credential_service.h"
 #include "endpoint.h"
 #include "endpoint_type.h"
 #include "ods_error.h"
 #include "transfer_service.h"
+
+#endif // ONEDATASHARE_ONEDATASHARE_H
