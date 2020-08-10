ec29c157a2d1ece9c0ce837d6d2262fd94250ed0
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-08-10 15:34:51 -0400

    Added more documentation for Credential_service_impl.

--- src/credential_service_impl.h
+++ src/credential_service_impl.h
@@ -37,14 +37,48 @@ public:
                             const std::string& ods_url,
                             std::unique_ptr<Rest> rest_caller);
 
+    /**
+     * Makes a REST API call to get the OAuth url needed to register an endpoint of the specified type.
+     *
+     * @param type the endpoint type to get the OAuth url for
+     *
+     * @return a string containing the OAuth url
+     *
+     * @exception Connection_error if unable to connect to One Data Share
+     * @exception Unexpected_response_error if an unexpected response is received from One Data Share
+     */
     std::string oauth_url(Oauth_endpoint_type type) const override;
 
+    /**
+     * Makes a REST API call to register the specified credentials.
+     *
+     * @param type the endpoint type to register
+     * @param cred_id borrowed reference to the credential identifier to associate with the registered endpoint
+     * @param uri borrowed reference to the uri of the endpoint to register
+     * @param username borrowed pointer to the username needed to log in to the endpoint or nullptr to register an
+     * endpoint without a username
+     * @param secret borrowed pointer to the password needed to log in to the endpoint or nullptr to register an
+     * endpoint without a password
+     *
+     * @exception Connection_error if unable to connect to One Data Share
+     * @exception Unexpected_response_error if an unexpected response is received from One Data Share
+     */
     void register_credential(Credential_endpoint_type type,
                              const std::string& cred_id,
                              const std::string& uri,
                              const std::string* username,
                              const std::string* secret) const override;
 
+    /**
+     * Makes a REST API call to get the list of credential identifiers of the specified type that are registered.
+     *
+     * @param type the endpoint type to list the registered credential identifiers of
+     *
+     * @return a vector of the registered credential identifiers for the sepcified endpoint
+     *
+     * @exception Connection_error if unable to connect to One Data Share
+     * @exception Unexpected_response_error if an unexpected response is received from One Data Share
+     */
     std::vector<std::string> credential_id_list(Endpoint_type type) const override;
 
 private:
