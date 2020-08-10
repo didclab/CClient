5806be6c0457e277f369ab37ce1286bd504cedab
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-08-10 10:44:35 -0400

    Added error_message.{h, cpp} for storing error message constants.

---
+++ src/error_message.cpp
@@ -0,0 +1,7 @@
+/*
+ * error_message.cpp
+ * Andrew Mikalsen
+ * 8/10/20
+ */
+
+#include "error_message.h"
---
+++ src/error_message.h
@@ -0,0 +1,10 @@
+/*
+ * error_message.h
+ * Andrew Mikalsen
+ * 8/10/20
+ */
+
+#ifndef ONEDATASHARE_ERROR_MESSAGE_H
+#define ONEDATASHARE_ERROR_MESSAGE_H
+
+#endif // ONEDATASHARE_ERROR_MESSAGE_H
