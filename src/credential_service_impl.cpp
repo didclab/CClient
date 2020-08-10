d9551d494a10089d485f4b2b8f6c9fd491d496ef
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-08-10 10:39:00 -0400

    Moved more error messages into constants in Credential_service_impl.

--- src/credential_service_impl.cpp
+++ src/credential_service_impl.cpp
@@ -28,21 +28,20 @@ constexpr auto api_path_oauth {"/api/oauth"};
 /** Path of the REST API call for credentials. */
 constexpr auto api_path_cred {"/api/cred"};
 
-/** Error message when a status code other than 303 is received when requesting an oauth url. */
-constexpr auto oauth_no_303_msg {"Expected a status 303 response code when requesting oauth url."};
+/** Error message when a 303 response code is expected and not received. */
+constexpr auto expect_303_msg {"Expected a status 303 response code."};
 
-/** Error message when there is no location in the response headers when requesting an oauth url. */
-constexpr auto oauth_no_location_msg {
-    "Response headers missing the expected \"Location\" header when reqesting oauth url."};
+/** Error message when a Location header is expected and not received. */
+constexpr auto expect_location_msg {"Expected a \"Location\" header in the response headers."};
 
-/** Error message when a status code other than 200 is received when registering credentials. */
-constexpr auto reg_cred_no_200_msg {"Expected a 200 response code when registering credentials."};
+/** Error message when a 200 status code is expected and not received. */
+constexpr auto expect_200_msg {"Expected a 200 response code when registering credentials."};
 
-/** Error message when converting an invalid Credential_endpoint_type to a string. */
-constexpr auto bad_cred_endpoint_msg {"Unknown Credential_endpoint_type."};
+/** Error message when using an undefined value of an enumeration. */
+constexpr auto unknown_enum_msg {"Unknown enumeration type."};
 
-/** Error message when converting an invalid Oauth_endpoint_type to a string. */
-constexpr auto bad_oauth_endpoint_msg {"Unknown Oauth_endpoint_type."};
+/** Error message when unable to parse the expected JSON response. */
+constexpr auto invalid_json_body_msg {"Unable to parse expected JSON response body."};
 
 /**
  * Converts a credential endpoint type to the string needed for the REST API.
@@ -62,10 +61,9 @@ std::string as_string(Credential_endpoint_type type)
         return "s3";
     case Credential_endpoint_type::sftp:
         return "sftp";
-    default:
-        // invalid enum
-        throw std::invalid_argument(bad_cred_endpoint_msg);
     }
+
+    throw std::invalid_argument(unknown_enum_msg);
 }
 
 /**
@@ -86,10 +84,9 @@ std::string as_string(Oauth_endpoint_type type)
         return "gftp";
     case Oauth_endpoint_type::google_drive:
         return "gdrive";
-    default:
-        // invalid enum
-        throw std::invalid_argument(bad_oauth_endpoint_msg);
     }
+
+    throw std::invalid_argument(unknown_enum_msg);
 }
 
 /**
@@ -131,27 +128,28 @@ Credential_service_impl::Credential_service_impl(const std::string& ods_auth_tok
       rest_caller_ {std::move(rest_caller)}
 {}
 
-std::string Credential_service_impl::oauth_url(const Oauth_endpoint_type type) const
+std::string Credential_service_impl::oauth_url(Oauth_endpoint_type type) const
 {
     // if get throws an exception, propagate it up
     const Response response {rest_caller_->get(ods_url_ + api_path_oauth + "?type=" + as_string(type), headers_)};
 
     if (response.status != 303) {
-        // unexpected response
-        throw Unexpected_response_error {oauth_no_303_msg, response.status};
+        throw Unexpected_response_error {expect_303_msg, response.status};
     }
 
-    // find url in a single location header, as there should only be one
+    // find url contained in a Location header (there should only be one Location header)
     const auto iter {response.headers.find("Location")};
+
+    // check that there was a Location header
     if (iter == response.headers.end()) {
-        // did not contain Location header
-        throw Unexpected_response_error {oauth_no_location_msg, response.status};
+        throw Unexpected_response_error {expect_location_msg, response.status};
     }
 
+    // return the url
     return iter->second;
 }
 
-void Credential_service_impl::register_credential(const Credential_endpoint_type type,
+void Credential_service_impl::register_credential(Credential_endpoint_type type,
                                                   const std::string& cred_id,
                                                   const std::string& uri,
                                                   const std::string* username,
@@ -163,8 +161,7 @@ void Credential_service_impl::register_credential(const Credential_endpoint_type
                                                 create_account_endpoint_credential(cred_id, uri, username, secret))};
 
     if (response.status != 200) {
-        // expected status 200
-        throw Unexpected_response_error {reg_cred_no_200_msg, response.status};
+        throw Unexpected_response_error {expect_200_msg, response.status};
     }
 }
 
@@ -174,23 +171,20 @@ std::vector<std::string> Credential_service_impl::credential_id_list(const Endpo
     const auto response {rest_caller_->get(ods_url_ + api_path_cred + "/" + as_string(type), headers_)};
 
     if (response.status != 200) {
-        // expected status 200
-        throw Unexpected_response_error {"Expected a status 200 response when fetching the credential id list.",
-                                         response.status};
+        throw Unexpected_response_error {expect_200_msg, response.status};
     }
 
-    // parse string array in CredList from response body
     simdjson::dom::parser parser {};
     std::vector<std::string> cred_list {};
+
     try {
+        // parse json string array in CredList json object from response body
         for (auto e : parser.parse(response.body).get_object().value()["credentialList"].get_array()) {
             cred_list.push_back(e.get_c_str().take_value());
         }
     } catch (simdjson::simdjson_error e) {
         // bad response body
-        throw Unexpected_response_error {
-            std::string("Failed to parse response body when fetching the credential id list: ") + e.what(),
-            response.status};
+        throw Unexpected_response_error {invalid_json_body_msg, response.status};
     }
 
     return cred_list;
