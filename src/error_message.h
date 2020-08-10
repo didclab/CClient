c391cf662d3e0b473700b8a3fe73c516fdd9ac5c
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-08-10 12:21:05 -0400

    Fixed error message constants by making error_message.h header only.

--- src/error_message.h
+++ src/error_message.h
@@ -11,19 +11,19 @@ namespace One_data_share {
 namespace Internal {
 
 /** Error message when a 303 response code is expected and not received. */
-constexpr char* expect_303_msg;
+constexpr auto expect_303_msg {"Expected a status 303 response code."};
 
 /** Error message when a Location header is expected and not received. */
-constexpr char* expect_location_msg;
+constexpr auto expect_location_msg {"Expected a \"Location\" header in the response headers."};
 
 /** Error message when a 200 status code is expected and not received. */
-constexpr char* expect_200_msg;
+constexpr auto expect_200_msg {"Expected a 200 response code when registering credentials."};
 
 /** Error message when using an undefined value of an enumeration. */
-constexpr char* unknown_enum_msg;
+constexpr auto unknown_enum_msg {"Unknown enumeration type."};
 
 /** Error message when unable to parse the expected JSON response. */
-constexpr char* invalid_json_body_msg;
+constexpr auto invalid_json_body_msg {"Unable to parse expected JSON response body."};
 
 } // namespace Internal
 } // namespace One_data_share
