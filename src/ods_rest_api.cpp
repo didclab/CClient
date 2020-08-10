8ee17c13c0934659ee08a80744814e586a6cbfa8
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-08-10 12:12:04 -0400

    Added ods_rest_api.{cpp,h} to store constants/utility specific to the rest api.

---
+++ src/ods_rest_api.cpp
@@ -0,0 +1,13 @@
+/*
+ * ods_rest_api.cpp
+ * Andrew Mikalsen
+ * 8/10/20
+ */
+
+#include "ods_rest_api.h"
+
+namespace One_data_share {
+namespace Internal {
+
+}
+} // namespace One_data_share
---
+++ src/ods_rest_api.h
@@ -0,0 +1,16 @@
+/*
+ * ods_rest_api.h
+ * Andrew Mikalsen
+ * 8/10/20
+ */
+
+#ifndef ONEDATASHARE_ODS_REST_API_H
+#define ONEDATASHARE_ODS_REST_API_H
+
+namespace One_data_share {
+namespace Internal {
+
+}
+} // namespace One_data_share
+
+#endif // ONEDATASHARE_ODS_REST_API_H
