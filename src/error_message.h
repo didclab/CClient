ef63e5ca08134b4506d64bda81522cf58ebc16f8
Andrew Mikalsen <ajmikals@buffalo.edu>
2020-08-11 09:00:12 -0400

    Removed periods from error messages.

--- src/error_message.h
+++ src/error_message.h
@@ -14,19 +14,19 @@ namespace Internal {
 namespace Err {
 
 /** Error message when a 303 response code is expected and not received. */
-constexpr auto expect_303_msg {"Expected a status 303 response code."};
+constexpr auto expect_303_msg {"Expected a status 303 response code"};
 
 /** Error message when a Location header is expected and not received. */
-constexpr auto expect_location_msg {"Expected a \"Location\" header in the response headers."};
+constexpr auto expect_location_msg {"Expected a \"Location\" header in the response headers"};
 
 /** Error message when a 200 status code is expected and not received. */
-constexpr auto expect_200_msg {"Expected a 200 response code when registering credentials."};
+constexpr auto expect_200_msg {"Expected a 200 response code when registering credentials"};
 
 /** Error message when using an undefined value of an enumeration. */
-constexpr auto unknown_enum_msg {"Unknown enumeration type."};
+constexpr auto unknown_enum_msg {"Unknown enumeration type"};
 
 /** Error message when unable to parse the expected JSON response. */
-constexpr auto invalid_json_body_msg {"Unable to parse expected JSON response body."};
+constexpr auto invalid_json_body_msg {"Unable to parse expected JSON response body"};
 
 } // namespace Err
 } // namespace Internal
